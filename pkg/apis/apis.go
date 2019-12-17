422637b4e12612124fbc224b88fae6d10a3bd0d3
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-12-17 14:48:10 +1000

    A controller to listen in on CRD changes

---
+++ pkg/apis/apis.go
@@ -0,0 +1,18 @@
+package apis
+
+import (
+	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// AddToSchemes may be used to add all resources defined in the project to a Scheme
+var AddToSchemes runtime.SchemeBuilder
+
+func init() {
+	AddToSchemes = append(AddToSchemes, apiextensions.AddToScheme)
+}
+
+// AddToScheme adds all Resources to the Scheme
+func AddToScheme(s *runtime.Scheme) error {
+	return AddToSchemes.AddToScheme(s)
+}
